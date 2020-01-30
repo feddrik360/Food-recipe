52ab7b12df9480013c20140fafc7f24dad751ec0
feddrik360 <fredddie360@outlook.com>
2020-01-30 08:10:53 +0000

    Updated all files

--- app.py
+++ app.py
@@ -8,29 +8,33 @@ from bson.objectid import ObjectId
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisisasecret!'
-app.config["MONGO_DBNAME"] = 'task_manager'
-app.config[
-    "MONGO_URI"] = 'mongodb+srv://Feddrik360:Office123@myfirstcluster-qdngs.mongodb.net/myProject?retryWrites=true&w=majority'
-# app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-# app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
+
 
 @app.route('/')
 def homepage():
     return render_template("index.html")
+
 
 @app.route('/recipe/<object_id>')
 def recipe(object_id):
     recipe = mongo.db.Cuisine.find_one({"_id": ObjectId(object_id)})
     return render_template('recipe.html', recipe=recipe)
 
+
 @app.route('/edit_recipe/<object_id>')
 def edit_recipe(object_id):
     recipe = mongo.db.Cuisine.find_one({"_id": ObjectId(object_id)})
     return render_template('edit_recipe.html', recipe=recipe)
+
 
 @app.route('/edited_recipe/<object_id>', methods=['POST'])
 def edited_recipe(object_id):
@@ -47,15 +51,18 @@ def edited_recipe(object_id):
     recipe = mongo.db.Cuisine.find_one({"_id": ObjectId(object_id)})
     return render_template('recipe.html', recipe=recipe)
 
+
 @app.route('/delete_recipe/<object_id>')
 def delete_recipe(object_id):
     mongo.db.Cuisine.remove({'_id': ObjectId(object_id)})
     recipes1 = mongo.db.Cuisine.find()
     return render_template('recipes.html', recipes=recipes1)
 
+
 @app.route('/addrecipe')
 def add_recipe():
     return render_template('add_recipe.html')
+
 
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
@@ -64,16 +71,16 @@ def insert_recipe():
     recipes1 = mongo.db.Cuisine.find()
     return render_template('recipes.html', recipes=recipes1)
 
+
 @app.route('/recipes')
 def recipes():
     recipes = mongo.db.Cuisine.find()
     return render_template("recipes.html", recipes=recipes)
+
 
 @app.route('/home')
 def home():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',
-            port=(os.environ.get('PORT')),
-            debug=True)
+    app.run(host='0.0.0.0',port=(os.environ.get('PORT')), debug=False)
